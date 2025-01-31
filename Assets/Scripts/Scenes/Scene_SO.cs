223f29e831326b4a4560d30d0c44b70f00d36268
Rui <xurui127@gmail.com>
2025-01-31 15:51:45 -0500

    Add a script

---
+++ Assets/Scripts/Scenes/Scene_SO.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[CreateAssetMenu(fileName = "NewScene", menuName = "Scene/NewScene")]
+public class Scene_SO : ScriptableObject
+{
+    public int id;
+    public Image scene;
+}
